577ee50940b3b6bcf0f6cf53b765f3a3c2bdeeb0
nascimento <rfonascimento@gmail.com>
2019-01-14 21:43:38 +0000

    commit

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes: Hero[];
+
+  selectedHero: Hero;
+
+  onSelect(hero: Hero): void{
+    this.selectedHero = hero;
+  }
+
+  constructor(private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+}
